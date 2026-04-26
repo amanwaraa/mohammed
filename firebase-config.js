07df23bc6bae889933c1bb0fb098126c96598ae1
Maroshop <homworkhhh25@gmail.com>
2026-04-26 21:27:12 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBEzyPkdTa2_LfRo0i-XjFYC3IEq-k6uak",
+    authDomain: "pppp-f75ad.firebaseapp.com",
+    databaseURL: "https://pppp-f75ad-default-rtdb.firebaseio.com",
+    projectId: "pppp-f75ad",
+    storageBucket: "pppp-f75ad.firebasestorage.app",
+    messagingSenderId: "5059631301",
+    appId: "1:5059631301:web:10f3217f56ff8df5efb8ac",
+    measurementId: "G-GZM0CZ8J5L"
+  },
+
+  firebaseRoot: "cashier_units_pro_pppp_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_pppp_885766842"
+  ]
+};
